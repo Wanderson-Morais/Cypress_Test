e7fd305392dac713b0e9dd94b81eaae189856fd5
Wanderson Morais <wandersonmoraisdesousa@gmail.com>
2026-03-31 17:04:39 -0400

    Adicionar testes para verificar a exibição de informações dos produtos na página da Bemol

--- cypress/e2e/FrontServeRest/bemol/produtos.cy.js
+++ cypress/e2e/FrontServeRest/bemol/produtos.cy.js
@@ -1,5 +1,64 @@
-describe('Bemol - Produtos', () => {
-  it('deve acessar a página da Bemol', () => {
+describe('Bemol - Cards de Produto', () => {
+  beforeEach(() => {
+    cy.on('uncaught:exception', () => false)
     cy.visit('https://www.bemol.com.br/')
+  })
+
+  it('CT-001 - deve exibir a imagem do produto no card', () => {
+    cy.get('.product-card img, .shelf-item img, [class*="product"] img')
+      .first()
+      .should('be.visible')
+      .and('have.attr', 'src')
+  })
+
+  it('CT-002 - deve exibir o nome do produto no card', () => {
+    cy.get('.product-card .product-name, .shelf-item__title, [class*="product"] [class*="name"], [class*="product"] [class*="title"]')
+      .first()
+      .should('be.visible')
+      .and('not.be.empty')
+  })
+
+  it('CT-003 - deve exibir a marca do produto no card', () => {
+    cy.get('[class*="brand"], [class*="marca"]')
+      .first()
+      .should('be.visible')
+      .and('not.be.empty')
+  })
+
+  it('CT-004 - deve exibir o preço promocional e opção de parcelamento no card', () => {
+    cy.get('[class*="price"], [class*="preco"], [class*="valor"]')
+      .first()
+      .should('be.visible')
+
+    cy.get('[class*="installment"], [class*="parcel"], [class*="parcela"]')
+      .first()
+      .should('be.visible')
+  })
+
+  it('CT-005 - botão "Mais detalhes" deve redirecionar para a página do produto', () => {
+    cy.contains('a, button', /mais detalhes/i)
+      .first()
+      .should('be.visible')
+      .click()
+
+    cy.url().should('not.eq', 'https://www.bemol.com.br/')
+  })
+
+  it('CT-006 - botão "Adicionar ao carrinho" deve adicionar o produto', () => {
+    cy.contains('button', /adicionar ao carrinho/i)
+      .first()
+      .click({ force: true })
+
+    cy.get('[class*="cart"] [class*="count"], [class*="carrinho"] [class*="count"], [class*="minicart"]')
+      .should('not.contain', '0')
+  })
+
+  it('CT-007 - clicar no nome do produto deve redirecionar para a página do produto', () => {
+    cy.get('[class*="product"] [class*="name"] a, [class*="product"] [class*="title"] a, .shelf-item__title a')
+      .first()
+      .should('be.visible')
+      .click()
+
+    cy.url().should('not.eq', 'https://www.bemol.com.br/')
   })
 })
